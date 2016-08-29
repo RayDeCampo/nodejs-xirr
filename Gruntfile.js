fc2184a371b1d0337178f126d690018bd7b4e1a6
rdecampo <ray@decampo.org>
2016-08-29 18:26:30 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+    require('jit-grunt')(grunt);
+
+    /////////////////////////// JSHINT ///////////////////////////////////////
+    grunt.config('jshint', {
+        options: {
+            eqeqeq: true,
+            node: true,
+            strict: true,
+            undef: true,
+            unused: 'vars'
+        },
+        all: ['**/*.js', '!node_modules/**']
+    });
+};
+
