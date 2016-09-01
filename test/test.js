4e3d4fda686c19ba5dd294c0b5abe0dd37c4cf33
rdecampo <ray@decampo.org>
2016-08-31 21:18:35 -0400

    Finished failure mode tests and added sinon and require-subvert module for stubbing the newton-raphson-method module.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@
 'use strict';
 
 var assert = require('assert');
+var requireSubvert = require('require-subvert')(__dirname);
+var sinon = require('sinon');
 var xirr = require('../');
 
 describe('xirr', function() {
@@ -22,8 +24,8 @@ describe('xirr', function() {
         assert.equal(0.10, result.toPrecision(6));
     });
 
-    it('computes the xirr on 1 year growth of 12% with investments at 3 ' + 
-            'month intervals the same as LibreOffice', function() {
+    it('computes the xirr on a particular data set ' +
+            'the same as a popular spreadsheet', function() {
         var transactions = [];
         transactions.push({ amount: -1000, when: new Date(2010,0,1) });
         transactions.push({ amount: -1000, when: new Date(2010,3,1) });
@@ -34,7 +36,7 @@ describe('xirr', function() {
         assert.equal(0.121268, result.toPrecision(6));
     });
 
-    it('computes the negative xirr on 1 year growth of 10%', function() {
+    it('computes the negative xirr on 1 year decline of 10%', function() {
         var transactions = [];
         transactions.push({ amount: -1000, when: new Date(2010,0,1) });
         transactions.push({ amount: 900, when: new Date(2011,0,1) });
@@ -43,14 +45,39 @@ describe('xirr', function() {
     });
 
     describe('failure modes', function() {
-        it('throws an exception when Newton\'s method fails');
+        it('throws an exception when Newton\'s method fails', sinon.test(function() {
+            var newtonStub = sinon.stub();
+            newtonStub.returns(false);
+            requireSubvert.subvert('newton-raphson-method', newtonStub);
+            var xirrWithStub = requireSubvert.require('../lib/xirr');
+            var transactions = [];
+            transactions.push({ amount: -1000, when: new Date(2010,0,1) });
+            transactions.push({ amount: 1100, when: new Date(2011,0,1) });
+            assert.throws(function() { xirrWithStub(transactions); },
+                /Newton-Raphson algorithm failed to converge./);
+            requireSubvert.cleanUp();
+        }));
         it('throws an exception when there are less than 2 transactions', function() {
-            var msg = 'Argument is not an array with length of 2 or more.';
-            assert.throws(xirr, Error, msg);
-            assert.throws(function() { xirr([]); }, Error, msg);
-            assert.throws(function() { xirr([{ amount: -1000, when: new Date(2010,0,1) }]); }, Error, msg);
+            var msg = /Argument is not an array with length of 2 or more./;
+            assert.throws(xirr, msg);
+            assert.throws(function() { xirr([]); }, msg);
+            assert.throws(function() { xirr([{ amount: -1000, when: new Date(2010,0,1) }]); }, msg);
         });
-        it('throws an exception when all transactions are at the same time');
-        it('throws an exception when all transactions are the same sign');
+        it('throws an exception when all transactions are on the same day', function() {
+            var transactions = [];
+            transactions.push({ amount: -1000, when: new Date(2010, 0, 1, 9) });
+            transactions.push({ amount: -1000, when: new Date(2010, 0, 1, 13) });
+            transactions.push({ amount: 2100, when: new Date(2010, 0, 1, 16) });
+            assert.throws(function() { xirr(transactions); },
+                /Transactions must not all be on the same day./);
+        });
+        it('throws an exception when all transactions are the same sign', function() {
+            var transactions = [];
+            transactions.push({ amount: -1000, when: new Date(2010, 0, 1) });
+            transactions.push({ amount: -1000, when: new Date(2010, 4, 1) });
+            transactions.push({ amount: -2000, when: new Date(2010, 8, 1) });
+            assert.throws(function() { xirr(transactions); },
+                /Transactions must not all be the same sign./);
+        });
     });
 });
